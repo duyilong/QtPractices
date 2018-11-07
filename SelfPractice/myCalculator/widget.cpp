7c08cc817c2fc4ff7fc61d819349b5a6f8a1b71d
duyilong <duyl_d@qq.com>
2018-11-07 22:26:08 +0800

    Add files via upload

--- SelfPractice/myCalculator/widget.cpp
+++ SelfPractice/myCalculator/widget.cpp
@@ -1,14 +1,84 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include "ui_widget.h"
+#include "QLineEdit"
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    // 绑定数字键和槽函数，为方便，将10个键绑定到一个槽函数
+    connect(ui->btn0,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn1,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn2,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn3,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn4,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn5,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn6,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn7,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn8,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
+    connect(ui->btn9,SIGNAL(clicked(bool)),this,SLOT(digit_clicked()));
 }
 
 Widget::~Widget()
 {
     delete ui;
 }
+
+
+void Widget::on_btnDot_clicked()
+{
+
+}
+
+void Widget::on_btnClear_clicked()
+{// 清空显示的字符和计算用的字符
+    strShow = "";
+    str1 = "";
+    str2 = "";
+    param1 = 0.0;
+    param2 = 0.0;
+    ui->lineEdit->setText(strShow);
+}
+
+void Widget::paramUpdate(QString str1, QString str2)
+{
+    param1 = str1.toDouble();
+    double var = str2.toDouble();
+    switch (lastOperation) {
+    case plus:
+        param1+=var;
+        break;
+    case minus:
+        param1-=var;
+        break;
+    case multiply:
+        param1*=var;
+        break;
+    case divide:
+        param1/=var;
+        break;
+    default:
+        break;
+    }
+}
+
+void Widget::on_btnPlus_clicked()
+{
+    if(lastOperation != 0)
+    {
+        paramUpdate(str1,str1);
+        str1 = QString("%1").arg(param1);
+        str2 = "";
+        strShow = str1 + "\n +  ";
+        lastOperation = plus;
+    }
+    else
+    {
+        lastOperation = plus; // 计算位标志设为＋
+        str1 = strShow;       // 运算数1设为当前显示的字符
+        str2 = "";
+        strShow += "\n +  ";      // 当前显示的字符下一行显示新的运算数
+    }
+}
+
--- SelfPractice/myCalculator/widget.h
+++ SelfPractice/myCalculator/widget.h
@@ -1,4 +1,4 @@
-#ifndef WIDGET_H
+﻿#ifndef WIDGET_H
 #define WIDGET_H
 
 #include <QWidget>
@@ -15,14 +15,24 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+private slots:
+    void digit_clicked();
+
+    void on_btnDot_clicked();
+
+    void on_btnClear_clicked();
+
+    void on_btnPlus_clicked();
+
 private:
-    Ui::Widget *ui;
-    QString strShow;
-    double   result;
-    QString    str1;
-    QString    str2;
-    int      param1;
-    int      param2;
+    Ui::Widget  *ui;
+private:
+//    double sunInMemory;//内存中保存的值
+    double sumSoFar;//积累到目前的结果，按=之后会计算最新值并且显示在屏幕上。按C会清零
+    double factorSoFar;
+    QString pendingAdditiveOperator;
+    QString pendingMultiplicativeOperator;
+    bool waitingForOperator;
 };
 
 #endif // WIDGET_H
